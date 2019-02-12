55514ff86a975316d8465e10004b51b5b3aaac48
Jason James Simpson <jasonjamessimpson@gmail.com>
2019-02-13 12:21:33 +1300

    search subreddit by topic button added, needs error warning

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,10 +2,38 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
 
-const LoadSubreddit = ({dispatch}) => (
-  <button onClick={() => dispatch(fetchPosts('newzealand'))}>
+// const LoadSubreddit = ({dispatch}) => (
+//   <button onClick={() => dispatch(fetchPosts('newzealand'))}>
+//     Fetch Posts
+//   </button>
+// )
+
+class LoadSubreddit extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      topic: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <p><input placeholder='Enter topic' name='topic' onChange={this.handleChange} value={this.state.topic}/></p>
+        <button onClick={() => this.props.dispatch(fetchPosts(this.state.topic))}>
     Fetch Posts
-  </button>
-)
+        </button>
+      </div>
+    )
+  }
+}
 
 export default connect()(LoadSubreddit)
